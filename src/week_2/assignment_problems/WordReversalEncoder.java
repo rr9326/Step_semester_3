66853e006912b35086a68f069f71ac042084eef4
rr9326 <rishithareddy520@gmail.com>
2026-08-29 13:16:46 +0530

    Assignment problem 2

---
+++ src/week_2/assignment_problems/WordReversalEncoder.java
@@ -0,0 +1,31 @@
+package week_2.assignment_problems;
+
+import java.util.Scanner;
+
+public class WordReversalEncoder {
+    static String reverseEachWord(String sentence) {
+String[] words = sentence.split(" ");
+StringBuilder result = new StringBuilder();
+for (int i = 0; i < words.length; i++) {
+StringBuilder reversedWord = new StringBuilder();
+// Reverse each word using a loop
+for (int j = words[i].length() - 1; j >= 0; j--) {
+reversedWord.append(words[i].charAt(j));
+}
+result.append(reversedWord);
+// Add space between words
+if (i < words.length - 1) {
+result.append(" ");
+}
+}
+return result.toString();
+}
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter a sentence: ");
+String sentence = sc.nextLine();
+String result = reverseEachWord(sentence);
+System.out.println("Reversed sentence: " + result);
+sc.close();
+}
+}
