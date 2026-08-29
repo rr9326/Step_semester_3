59fbdfed8724c74e8fb10a397074ff9dc0643d0b
rr9326 <rishithareddy520@gmail.com>
2026-08-29 21:37:57 +0530

    Week 2 Problem 2

---
+++ src/week_2/class_problems/VowelConsonantCounter.java
@@ -0,0 +1,48 @@
+package week_2.class_problems;
+
+import java.util.Scanner;
+
+public class VowelConsonantCounter {
+      public static void countVowelsAndConsonants(String text) {
+
+        int vowels = 0;
+        int consonants = 0;
+
+        text = text.toLowerCase();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char ch = text.charAt(i);
+
+            // Ignore spaces
+            if (ch == ' ') {
+                continue;
+            }
+
+            // Check for vowels
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+
+                vowels++;
+
+            } else {
+                consonants++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels +
+                           " | Consonants: " + consonants);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        countVowelsAndConsonants(text);
+
+        sc.close();
+    }
+}
