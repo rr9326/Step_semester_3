8aa08818d97c88d65c9c4d114d3b35b4a72ded8d
rr9326 <rishithareddy520@gmail.com>
2026-08-29 19:50:52 +0530

    Week 4 Question 2

---
+++ src/week_4/assignment_problems/MaxSubArray.java
@@ -0,0 +1,44 @@
+package week_4.assignment_problems;
+
+import java.util.Scanner;
+
+public class MaxSubArray {
+      public static int maxSubArray(int[] nums) {
+
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            // Either start fresh or continue previous subarray
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            // Update maximum sum
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = maxSubArray(nums);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+
+        sc.close();
+    }
+}
